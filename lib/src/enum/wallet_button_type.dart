dfeba66d1ce4d300eb3484d63973a351a4cad17f
monocsp <monocsp@naver.com>
2024-03-19 17:30:38 +0900

    [update] add assets
    add Samsung Wallet assets, png svg

--- lib/src/enum/wallet_button_type.dart
+++ lib/src/enum/wallet_button_type.dart
@@ -1,33 +1,44 @@
-///
-/// Samsung Wallet Button Style enum.
-/// @enum {string}
-///
-enum SamsungWalletButtonStyle {
-  ///
-  /// Samsung Wallet Button Type Normal.
-  ///
-  /// @example https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card.png
-  ///
-  normal,
 
-  ///
-  /// Samsung Wallet Button Type Normal For Dark Theme.
-  ///
-  /// @example https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card-dark.png
-  ///
-  normalDark,
 
-  ///
-  /// Samsung Wallet Button Type Vertical.
-  ///
-  /// @example https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card-ver.png
-  ///
-  vertical,
+/// Samsung Wallet Button Design 
 
-  ///
-  /// Samsung Wallet Button Type Vertical For Dark Theme.
-  ///
-  /// @example https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card-ver-dark.png
-  ///
-  verticalDark
+enum ButtonDesignType {
+/// Add To Samsung Wallet Button
+/// 
+/// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/add-to-samsung-wallet_en.png)
+  aT,
+  /// Basic Samsung Wallet Button
+  /// 
+  /// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card.png)
+  basic,
+  /// Add To Samsung Wallet Button with Icon
+  
+  icon_AT,
+  /// Basic Samsung Wallet Button with Icon
+   icon_basic,
+}
+
+
+
+enum ButtonTextPositionType {
+
+  /// Horizontal Samsung Wallet Button
+  /// 
+  /// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card-ver.png)
+  hor,
+  /// Vertical Samsung Wallet Button
+  /// 
+  /// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card.png)
+  ver,
+}
+
+enum ButtonThemeType {
+/// For Light Theme Samsung Wallet Button
+  /// 
+  /// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card.png)
+  pos,
+  /// For Dark Theme Samsung Wallet Button
+  /// 
+  /// Image [Sample](https://d3unf4s5rp9dfh.cloudfront.net/SamsungWallet_doc/wallet-card-dark.png)
+  rev,
 }
